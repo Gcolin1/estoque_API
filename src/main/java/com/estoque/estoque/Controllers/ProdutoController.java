af4d2d29c49e09d0c6d27895f7d4b7c1d1ffd49e
Gcolin1 <guilhermecolin09@gmail.com>
2023-07-31 00:11:47 -0300

    crud estoque

--- src/main/java/com/estoque/estoque/Controllers/ProdutoController.java
+++ src/main/java/com/estoque/estoque/Controllers/ProdutoController.java
@@ -3,14 +3,14 @@ package com.estoque.estoque.Controllers;
 import com.estoque.estoque.Produto.ListaProdutoDTO;
 import com.estoque.estoque.Produto.Produto;
 import com.estoque.estoque.Produto.ProdutoRepository;
+import com.estoque.estoque.Produto.dadosCadastroProdutosDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/produtos")
@@ -19,11 +19,56 @@ public class ProdutoController {
     @Autowired
     private ProdutoRepository repository;
 
+    //LISTAR TODOS OS PRODUTOS
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
     public List<ListaProdutoDTO> getAll(){
         List<ListaProdutoDTO> productList = repository.findAll().stream().map(ListaProdutoDTO::new).toList();
         return productList;
+    }
+
+    //CRIAR PRODUTO
+    @PostMapping
+    public void CreateProduct(@RequestBody dadosCadastroProdutosDTO dados){
+        Produto produtoDados = new Produto(dados);
+        repository.save(produtoDados);
+        return;
+    }
+
+    //RETIRADA DE ESTOQUE
+    @PutMapping("/{id}")
+    public ResponseEntity<Produto> RetirarEstoque(@PathVariable Long id, @RequestBody Produto produtoAtualizado){
+        Optional<Produto> ProdutoExistenteOptional = repository.findById(id);
+
+        if(!ProdutoExistenteOptional.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+
+        Produto produtoExistente = ProdutoExistenteOptional.get();
+
+        produtoExistente.setEstoque(produtoExistente.getEstoque() - produtoAtualizado.getEstoque());
+
+        Produto produtoAtualizadoSalvo = repository.save(produtoExistente);
+
+        return ResponseEntity.ok(produtoAtualizadoSalvo);
+    }
+
+    //REPOR ESTOQUE
+    @PutMapping("repor/{id}")
+    public ResponseEntity<Produto> AdicionarEstoque(@PathVariable Long id, @RequestBody Produto produtoAtualizado){
+        Optional<Produto> ProdutoExistenteOptional = repository.findById(id);
+
+        if(!ProdutoExistenteOptional.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+
+        Produto produtoExistente = ProdutoExistenteOptional.get();
+
+        produtoExistente.setEstoque(produtoExistente.getEstoque() + produtoAtualizado.getEstoque());
+
+        Produto produtoAtualizadoSalvo = repository.save(produtoExistente);
+
+        return ResponseEntity.ok(produtoAtualizadoSalvo);
     }
 
 }
